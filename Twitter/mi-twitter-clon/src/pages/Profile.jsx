52a979268562f0b537ed94cc965150a667c1432f
Monica <monica.espinosa.hernandez508@gmail.com>
2025-06-30 23:57:26 -0500

    Proyecto Final Intro a React

---
+++ Twitter/mi-twitter-clon/src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = ({ user }) => {
+  return (
+    <div>
+      <h1>Perfil</h1>
+      {user && <p>Nombre de usuario: {user.username}</p>}
+    </div>
+  );
+};
+
+export default Profile;
